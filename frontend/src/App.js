3662b85e1586327fc9156e4749eee297359b7ee2
Robert Vari <mail.robertvari@gmail.com>
2024-10-20 17:06:29 +0200

    Basic components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import Navbar from "./components/Navbar"
+import LandingPage from "./components/LandingPage"
+import Footer from "./components/Footer"
+
 function App() {
   return (
     <div className="App">
-
+      <Navbar/>
+      <LandingPage/>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from "./Sidebar"
+import MovieListView from "./MovieListView"
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Sidebar/>
+      <MovieListView/>
+    </div>
+  )
+}
