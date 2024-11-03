4ecfe419d09127592964d780771a559997df9f96
Robert Vari <mail.robertvari@gmail.com>
2024-11-03 18:14:06 +0100

    update

--- frontend/src/components/MovieListView.js
+++ frontend/src/components/MovieListView.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MovieCard from './micro/MovieCard'
+import axios from 'axios'
 
 export default function MovieListView() {
+  const [movies, set_movies] = useState([])
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:3001/movies"
+    }).then(res => set_movies(res.data))
+  }, [])
+
   return (
     <div className='listview'>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
+      {
+        movies.map(movie_data => <MovieCard movie_data={movie_data}/>)
+      }
     </div>
   )
 }
--- frontend/src/components/micro/MovieCard.js
+++ frontend/src/components/micro/MovieCard.js
@@ -3,17 +3,17 @@ import Poster from "../../images/poster.jpg"
 import PopularityProgress from "./PopularityProgress"
 import { Link } from 'react-router-dom'
 
-export default function MovieCard() {
+export default function MovieCard({movie_data}) {
   return (
     <Link to="/movies/12345-the-wild-robot" className='card movie-card'>
         <div className='poster-container'>
-            <img src={Poster} alt="" />
+            <img src={movie_data.poster} alt="" />
             <PopularityProgress/>
         </div>
         
         <div className='title-container'>
-            <h4>The Wild Robot</h4>
-            <small>Sep 12, 2024</small>
+            <h4>{movie_data.title}</h4>
+            <small>{movie_data.date}</small>
         </div>
     </Link>
   )
