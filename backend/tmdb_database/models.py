3843166f5bf244efcdb54330587c43d2f9a3741e
Robert Vari <mail.robertvari@gmail.com>
2024-11-17 15:34:53 +0100

    Pillow installed

--- backend/tmdb_database/models.py
+++ backend/tmdb_database/models.py
@@ -11,3 +11,15 @@ class SortItem(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Movie(models.Model):
+    tmdb_id = models.IntegerField()
+    genres = models.ManyToManyField(Genre)
+    title = models.CharField(max_length=200)
+    release_date = models.DateField()
+    poster_path = models.ImageField()
+    backdrop_path = models.ImageField()
+    vote_average = models.FloatField()
+    popularity = models.FloatField()
+    overview = models.TextField(max_length=2000)
+    language = models.CharField(max_length=200)
