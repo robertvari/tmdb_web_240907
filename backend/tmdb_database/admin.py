c184d913bf6557f120a3e91dc1915d58f25540a7
Robert Vari <mail.robertvari@gmail.com>
2024-11-17 16:00:07 +0100

    update

--- backend/tmdb_database/admin.py
+++ backend/tmdb_database/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Genre, SortItem
+from .models import Genre, SortItem, Movie
 
 admin.site.register(Genre)
 admin.site.register(SortItem)
+admin.site.register(Movie)
--- backend/tmdb_database/models.py
+++ backend/tmdb_database/models.py
@@ -11,15 +11,19 @@ class SortItem(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 class Movie(models.Model):
     tmdb_id = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     title = models.CharField(max_length=200)
     release_date = models.DateField()
-    poster_path = models.ImageField()
-    backdrop_path = models.ImageField()
-    vote_average = models.FloatField()
-    popularity = models.FloatField()
-    overview = models.TextField(max_length=2000)
-    language = models.CharField(max_length=200)
+    poster_path = models.ImageField(blank=True)
+    backdrop_path = models.ImageField(blank=True)
+    vote_average = models.FloatField(blank=True, default=0)
+    popularity = models.FloatField(blank=True, default=0)
+    overview = models.TextField(max_length=2000, blank=True)
+    language = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.title
